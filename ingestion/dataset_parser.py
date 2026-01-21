4721b60531e72959a97fd5798eb31e02dd595c26
Yosr <yosrasouaifi@gmail.com>
2026-01-22 02:10:36 +0300

    Create dataset_parser.py

---
+++ ingestion/dataset_parser.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_dataset(file_path):
+    if file_path.endswith('.csv'):
+        return pd.read_csv(file_path)
+    elif file_path.endswith('.json'):
+        return pd.read_json(file_path)
+    else:
+        raise ValueError("Unsupported file format")
